a47258cb994d6f16769ca7c93bf93162dce1732c
HAOYUAN-DENG <H.Deng12@newcastle.ac.uk>
2025-02-27 17:47:47 +0000

    standard Binary Search solution

---
+++ src/Question704BinarySearch/Solution.java
@@ -0,0 +1,31 @@
+package Question704BinarySearch;
+
+public class Solution {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, target);
+    }
+
+
+    public int binarySearch(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+
+        while (i <= j) {
+
+            int middleIndex = i + j >>> 1;
+            if (nums[middleIndex] > target) {
+                j = middleIndex - 1;
+            } else if (nums[middleIndex] < target) {
+                i = middleIndex + 1;
+            } else {
+                return middleIndex;
+            }
+
+        }
+
+        return -1; // number do not exist in loop
+    }
+
+
+}
+

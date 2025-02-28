80a398170b713ce93eb369d45c9820fbe72cda95
HAOYUAN-DENG <H.Deng12@newcastle.ac.uk>
2025-02-28 18:20:00 +0000

    Data structure--ArrayList implement by Array

---
+++ src/DataStructure/Array/DynamicArray.java
@@ -0,0 +1,77 @@
+package DataStructure.Array;
+
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+public class DynamicArray  {
+
+    private int size=0; // 逻辑大小
+    private int capacity=8;// 容量
+    private int[] array = {}; //new int[capacity];
+
+
+    public void addLast(int element) {
+        checkAndGrowCapacity();
+        array[size] = element;
+        size++;
+    }
+
+    public void add(int index, int element) {
+        checkAndGrowCapacity();
+
+        // Append element logic
+        if (index >= 0 && index < size) {
+            System.arraycopy(array, index, array, index+1, size-index);
+            array[index] = element;
+            size++;
+        } else if (index == size) {// 同等 allLast 情况
+            array[size] = element;
+            size++;
+        }
+
+
+
+    }
+
+    private void checkAndGrowCapacity() {
+        // capacity check
+        if (size == 0){
+            array = new int[capacity];
+        }
+
+        // Capacity check, if array is full then increase capacity ---> size = capacity
+        if (size == capacity){
+            // increase
+            int halfVolume = capacity >> 1;
+            capacity = capacity + halfVolume;
+            int[]newArray = new int[capacity];
+            System.arraycopy(array,0,newArray,0,size);
+            array = newArray;
+
+        }
+    }
+
+
+    public int get(int index) {
+        return array[index];
+    }
+
+    public void foreach(Consumer<Integer> consumer){
+        for (int i = 0; i < size; i++) {
+           // System.out.println(i);
+            consumer.accept(array[i]);
+
+        }
+    }
+
+
+    public int remove(int index){
+        int removedElement = array[index];
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
+        size--;
+        return removedElement;
+    }
+
+
+
+}
---
+++ src/DataStructure/Array/TestDynamicArray.java
@@ -0,0 +1,71 @@
+package DataStructure.Array;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
+public class TestDynamicArray {
+
+    @Test
+    public  void test1(){
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.addLast(1);
+        dynamicArray.addLast(2);
+        dynamicArray.addLast(3);
+        dynamicArray.addLast(4);
+
+        dynamicArray.add(2,5);
+
+        for (int i=0; i<5; i++ ){
+            System.out.println(dynamicArray.get(i));
+        }
+
+    }
+
+    @Test
+    public  void test2(){
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.addLast(1);
+        dynamicArray.addLast(2);
+        dynamicArray.addLast(3);
+        dynamicArray.addLast(4);
+
+        dynamicArray.foreach(element -> System.out.println(element) );
+    }
+
+    @Test
+    @DisplayName("Test Remove")
+    public void test3(){
+        DynamicArray dynamicArray = new DynamicArray();
+        dynamicArray.addLast(1);
+        dynamicArray.addLast(2);
+        dynamicArray.addLast(3);
+        dynamicArray.addLast(4);
+        dynamicArray.addLast(5);
+
+        int removed = dynamicArray.remove(2);
+        assertEquals(3, removed);
+        System.out.println("--------------");
+
+
+
+        dynamicArray.foreach(element -> System.out.println(element));
+    }
+
+    @Test
+    @DisplayName("Test increase volume")
+    public void test4(){
+        DynamicArray dynamicArray = new DynamicArray();
+        for (int i = 0; i < 9; i++) {
+            dynamicArray.addLast(i+1);
+        }
+
+        dynamicArray.foreach(element -> System.out.println(element));
+
+    }
+
+}
